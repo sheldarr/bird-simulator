3e8ff42a433f8bd82aac076008556f616c86e66e
Kewin Polok <sheldar@wp.pl>
2014-06-11 21:41:21 +0200

    Delete non used stuff

--- Utilities/Shapes/WorldCube.cs
+++ Utilities/Shapes/WorldCube.cs
@@ -9,12 +9,6 @@ namespace Utilities.Shapes
         private int Size { get; set; }
 
         private readonly int _skybox = Textures.Textures.Skybox;
-        private readonly int _skyboxUp = Textures.Textures.Skybox;
-        private readonly int _skyboxLeft = Textures.Textures.Skybox;
-        private readonly int _skyboxFront = Textures.Textures.Skybox;
-        private readonly int _skyboxRight = Textures.Textures.Skybox;
-        private readonly int _skyboxBack = Textures.Textures.Skybox;
-        private readonly int _skyboxDown = Textures.Textures.Skybox;
 
         public WorldCube(int size)
         {
@@ -35,7 +29,7 @@ namespace Utilities.Shapes
             GL.TexCoord2(0.5, 0.33); GL.Vertex3(Size, Size, Size);
             GL.End();
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxLeft);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0, 0.66); GL.Vertex3(-Size, -Size, -Size);
             GL.TexCoord2(0, 0.35); GL.Vertex3(-Size, Size, -Size);
@@ -43,7 +37,7 @@ namespace Utilities.Shapes
             GL.TexCoord2(0.25, 0.66); GL.Vertex3(Size, -Size, -Size);
             GL.End();
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxFront);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0.25, 0.66); GL.Vertex3(Size, -Size, -Size);
             GL.TexCoord2(0.5, 0.66); GL.Vertex3(Size, -Size, Size);
@@ -51,7 +45,7 @@ namespace Utilities.Shapes
             GL.TexCoord2(0.25, 0.35); GL.Vertex3(Size, Size, -Size);
             GL.End();
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxRight);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0.5, 0.66); GL.Vertex3(Size, -Size, Size);
             GL.TexCoord2(0.75, 0.66); GL.Vertex3(-Size, -Size, Size);
@@ -59,7 +53,7 @@ namespace Utilities.Shapes
             GL.TexCoord2(0.5, 0.35); GL.Vertex3(Size, Size, Size);
             GL.End();
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxBack);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0.75, 0.66); GL.Vertex3(-Size, -Size, Size);
             GL.TexCoord2(0.75, 0.35); GL.Vertex3(-Size, Size, Size);
@@ -67,7 +61,7 @@ namespace Utilities.Shapes
             GL.TexCoord2(1, 0.66); GL.Vertex3(-Size, -Size, -Size);
             GL.End();
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxDown);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
             GL.TexCoord2(0.25, 1); GL.Vertex3(-Size, -Size, Size);
             GL.TexCoord2(0.5, 1); GL.Vertex3(Size, -Size, Size);
