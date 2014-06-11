7db3a930ac3f726cd56a0d4e4085c9e9598fa462
Kewin Polok <sheldar@wp.pl>
2014-06-11 21:24:52 +0200

    Better skybox mapping

--- Utilities/Shapes/WorldCube.cs
+++ Utilities/Shapes/WorldCube.cs
@@ -8,12 +8,13 @@ namespace Utilities.Shapes
     {
         private int Size { get; set; }
 
-        private readonly int _skyboxUp = Textures.Textures.SkyboxUp;
-        private readonly int _skyboxLeft = Textures.Textures.SkyboxLeft;
-        private readonly int _skyboxFront = Textures.Textures.SkyboxFront;
-        private readonly int _skyboxRight = Textures.Textures.SkyboxRight;
-        private readonly int _skyboxBack = Textures.Textures.SkyboxBack;
-        private readonly int _skyboxDown = Textures.Textures.SkyboxDown;
+        private readonly int _skybox = Textures.Textures.Skybox;
+        private readonly int _skyboxUp = Textures.Textures.Skybox;
+        private readonly int _skyboxLeft = Textures.Textures.Skybox;
+        private readonly int _skyboxFront = Textures.Textures.Skybox;
+        private readonly int _skyboxRight = Textures.Textures.Skybox;
+        private readonly int _skyboxBack = Textures.Textures.Skybox;
+        private readonly int _skyboxDown = Textures.Textures.Skybox;
 
         public WorldCube(int size)
         {
@@ -26,52 +27,52 @@ namespace Utilities.Shapes
             GL.PushMatrix();
             GL.Enable(EnableCap.Texture2D);
 
-            GL.BindTexture(TextureTarget.Texture2D, _skyboxUp);
+            GL.BindTexture(TextureTarget.Texture2D, _skybox);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(0, 1); GL.Vertex3(-Size, Size, Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(Size, Size, Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(Size, Size, -Size);
-            GL.TexCoord2(1, 1); GL.Vertex3(-Size, Size, -Size);
+            GL.TexCoord2(0.25, 0.33); GL.Vertex3(Size, Size, -Size);
+            GL.TexCoord2(0.25, 0); GL.Vertex3(-Size, Size, -Size);
+            GL.TexCoord2(0.5, 0); GL.Vertex3(-Size, Size, Size);
+            GL.TexCoord2(0.5, 0.33); GL.Vertex3(Size, Size, Size);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, _skyboxLeft);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(0, 1); GL.Vertex3(-Size, -Size, -Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(-Size, Size, -Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(Size, Size, -Size);
-            GL.TexCoord2(1, 1); GL.Vertex3(Size, -Size, -Size);
+            GL.TexCoord2(0, 0.66); GL.Vertex3(-Size, -Size, -Size);
+            GL.TexCoord2(0, 0.35); GL.Vertex3(-Size, Size, -Size);
+            GL.TexCoord2(0.25, 0.35); GL.Vertex3(Size, Size, -Size);
+            GL.TexCoord2(0.25, 0.66); GL.Vertex3(Size, -Size, -Size);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, _skyboxFront);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(1, 1); GL.Vertex3(Size, -Size, Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(Size, Size, Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(Size, Size, -Size);
-            GL.TexCoord2(0, 1); GL.Vertex3(Size, -Size, -Size);
+            GL.TexCoord2(0.25, 0.66); GL.Vertex3(Size, -Size, -Size);
+            GL.TexCoord2(0.5, 0.66); GL.Vertex3(Size, -Size, Size);
+            GL.TexCoord2(0.5, 0.35); GL.Vertex3(Size, Size, Size);
+            GL.TexCoord2(0.25, 0.35); GL.Vertex3(Size, Size, -Size);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, _skyboxRight);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(1, 1); GL.Vertex3(-Size, -Size, Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(-Size, Size, Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(Size, Size, Size);
-            GL.TexCoord2(0, 1); GL.Vertex3(Size, -Size, Size);
+            GL.TexCoord2(0.5, 0.66); GL.Vertex3(Size, -Size, Size);
+            GL.TexCoord2(0.75, 0.66); GL.Vertex3(-Size, -Size, Size);
+            GL.TexCoord2(0.75, 0.35); GL.Vertex3(-Size, Size, Size);
+            GL.TexCoord2(0.5, 0.35); GL.Vertex3(Size, Size, Size);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, _skyboxBack);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(0, 1); GL.Vertex3(-Size, -Size, Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(-Size, Size, Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(-Size, Size, -Size);
-            GL.TexCoord2(1, 1); GL.Vertex3(-Size, -Size, -Size);
+            GL.TexCoord2(0.75, 0.66); GL.Vertex3(-Size, -Size, Size);
+            GL.TexCoord2(0.75, 0.35); GL.Vertex3(-Size, Size, Size);
+            GL.TexCoord2(1, 0.35); GL.Vertex3(-Size, Size, -Size);
+            GL.TexCoord2(1, 0.66); GL.Vertex3(-Size, -Size, -Size);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, _skyboxDown);
             GL.Begin(PrimitiveType.Quads);
-            GL.TexCoord2(1, 1); GL.Vertex3(-Size, -Size, Size);
-            GL.TexCoord2(1, 0); GL.Vertex3(Size, -Size, Size);
-            GL.TexCoord2(0, 0); GL.Vertex3(Size, -Size, -Size);
-            GL.TexCoord2(0, 1); GL.Vertex3(-Size, -Size, -Size);
+            GL.TexCoord2(0.25, 1); GL.Vertex3(-Size, -Size, Size);
+            GL.TexCoord2(0.5, 1); GL.Vertex3(Size, -Size, Size);
+            GL.TexCoord2(0.5, 0.66); GL.Vertex3(Size, -Size, -Size);
+            GL.TexCoord2(0.25, 0.66); GL.Vertex3(-Size, -Size, -Size);
             GL.End();
 
             GL.Disable(EnableCap.Texture2D);
--- Utilities/Textures/Textures.cs
+++ Utilities/Textures/Textures.cs
@@ -2,16 +2,6 @@
 {
     public static class Textures
     {
-        public static int Grass = TextureLoader.LoadTexture(@"Resources\grassTexture.jpg");
-        public static int Horizon = TextureLoader.LoadTexture(@"Resources\horizonTexture.jpg");
-        public static int Sky = TextureLoader.LoadTexture(@"Resources\skyTexture.jpg");
-        public static int Leafs = TextureLoader.LoadTexture(@"Resources\leafsTexture.jpg");
         public static int Skybox = TextureLoader.LoadTexture(@"Resources\skybox.jpg");
-        public static int SkyboxUp = TextureLoader.LoadTexture(@"Resources\skyboxUp.jpg");
-        public static int SkyboxLeft = TextureLoader.LoadTexture(@"Resources\skyboxLeft.jpg");
-        public static int SkyboxFront = TextureLoader.LoadTexture(@"Resources\skyboxFront.jpg");
-        public static int SkyboxRight = TextureLoader.LoadTexture(@"Resources\skyboxRight.jpg");
-        public static int SkyboxBack = TextureLoader.LoadTexture(@"Resources\skyboxBack.jpg");
-        public static int SkyboxDown = TextureLoader.LoadTexture(@"Resources\skyboxDown.jpg");
     }
 }
